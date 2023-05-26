98447e3076a3e648f999e5434301450001ad853c
Leo Herrmann <leo.herrmann@stud.h-da.de>
2023-05-26 10:52:39 +0200

    Navigation zum und vom ProductDetailsFragment repariert.

--- app/src/main/java/com/example/appactivitys/CustomerMain.java
+++ app/src/main/java/com/example/appactivitys/CustomerMain.java
@@ -27,16 +27,19 @@ public class CustomerMain extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_cart, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_customer_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_customer_main);
+        return navController.navigateUp() || super.onSupportNavigateUp();
+    }
 }
