e047f419bf23fb28bcd9fc5e0722d5e8f03362a5
Torsten <torsten.fahrner@stud.h-da.de>
2023-06-19 18:51:14 +0200

    toggle button feuert eine funktion ab mit true und false

--- app/src/main/java/com/example/blink/ui/ProductDetailsFragment.java
+++ app/src/main/java/com/example/blink/ui/ProductDetailsFragment.java
@@ -3,12 +3,15 @@ package com.example.blink.ui;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -38,6 +41,19 @@ public class ProductDetailsFragment extends Fragment {
                 addToCart();
             }
         });
+        ToggleButton toggleButton = binding.toggleButton;
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isChecked = toggleButton.isChecked();
+                if (isChecked) {
+                    Log.d("toggle", "onClick: true");
+                } else {
+                    Log.d("toggle", "onClick: false");
+                }
+            }
+        });
+
 
         return root;
     }
