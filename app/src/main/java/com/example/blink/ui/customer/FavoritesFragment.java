cfa405252d20281011bf01434b28e86b07f8ae36
Leo Herrmann <leo.herrmann@stud.h-da.de>
2023-06-20 12:54:25 +0200

    Favoriten werden im Favoriten-Fragment angezeigt

--- app/src/main/java/com/example/blink/ui/customer/FavoritesFragment.java
+++ app/src/main/java/com/example/blink/ui/customer/FavoritesFragment.java
@@ -7,14 +7,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.blink.R;
+import com.example.blink.database.AppDatabase;
+import com.example.blink.database.entities.Product;
+import com.example.blink.databinding.FragmentCustomerFavoritesBinding;
+
+import java.util.List;
 
 public class FavoritesFragment extends Fragment {
+    FragmentCustomerFavoritesBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_favorites, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCustomerFavoritesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        AppDatabase db = AppDatabase.getInstance(requireContext().getApplicationContext());
+        List<Product> products = db.productDao().GetFavoriteProducts();
+
+        for (Product product : products) {
+            View productView = getLayoutInflater().inflate(R.layout.sample_customer_favorites_product_view, null);
+            TextView nameTextView = productView.findViewById(R.id.nameTextView);
+            TextView priceTextView = productView.findViewById(R.id.priceTextView);
+            TextView supplierTextView = productView.findViewById(R.id.supplierTextView);
+
+            nameTextView.setText(product.name);
+            priceTextView.setText(String.format("%.2f€", product.price));
+            supplierTextView.setText(product.supplierName);
+
+            /*productView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    launchProductDetails(v);
+                }
+            });*/
+
+            binding.favoritesContainer.addView(productView);
+        }
+
+        return root;
     }
 }
